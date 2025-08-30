b0d5ba792c19769157fbed2d04aef0d99c9aa41c
Floridae242 <naruephonyotmao@gmail.com>
2025-08-30 22:33:54 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tracechain.db")
+DATABASE_URL = "sqlite:///./tracechain.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
